30f13390efe161b6af8433c445fcc43e5ed059ab
Hendra Manudinata <manoedinata@gmail.com>
2026-09-21 19:12:43 +0700

    feat: add done feature

--- script.js
+++ script.js
@@ -8,23 +8,26 @@ form.addEventListener('submit', function (event) {
     const todoText = input.value.trim();
     const descriptionInput = document.getElementById('todo-description');
     const todoDescription = descriptionInput.value.trim();
-
     if (todoText === '' || todoDescription === '') {
         alert('Lengkapin input tugas.');
         return;
     }
 
+    // container untuk setiap todo item
     let todoList = document.createElement('div');
     todoList.id = 'todo-list';
 
+    // judul todo
     let h3 = document.createElement('h3');
     h3.textContent = todoText;
     todoList.appendChild(h3);
 
+    // deskripsi todo
     let p = document.createElement('p');
     p.textContent = todoDescription;
     todoList.appendChild(p);
 
+    // tombol delete
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'delete-btn';
@@ -33,10 +36,29 @@ form.addEventListener('submit', function (event) {
     });
     todoList.appendChild(deleteButton);
 
+    // tombol done
+    const doneButton = document.createElement('button');
+    doneButton.textContent = 'Done';
+    doneButton.className = 'done-btn';
+    doneButton.addEventListener('click', function () {
+        if (h3.style.textDecoration === 'line-through') {
+            h3.style.textDecoration = 'none';
+            h3.style.color = 'black';
+            p.style.textDecoration = 'none';
+            p.style.color = 'black';
+        } else {
+            h3.style.textDecoration = 'line-through';
+            h3.style.color = 'gray';
+            p.style.textDecoration = 'line-through';
+            p.style.color = 'gray';
+        }
+    });
+    todoList.appendChild(doneButton);
 
     // tambahin todoList ke todoContainer di posisi paling atas
     todoContainer.insertBefore(todoList, todoContainer.firstChild);
 
+    // reset input
     input.value = '';
     descriptionInput.value = '';
     alert('Tugas ditambahkan!');
